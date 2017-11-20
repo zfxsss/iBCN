5143d8b42826a067aebc6126242daa0a04b63c3d
jxu <jxu@SPARE04>
2017-11-20 16:36:59 -0400

    add output test project

---
+++ ObjectPropertiesIterationTests/PropertiesIteratorTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ObjectPropertiesIteration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPropertiesIteration.Tests
+{
+    [TestClass()]
+    public class PropertiesIteratorTests
+    {
+        [TestMethod()]
+        public void PrintIterationTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
