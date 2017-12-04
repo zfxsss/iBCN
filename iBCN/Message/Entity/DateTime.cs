b14ecba93c506d10180482f61e975acacc9c591d
jxu <jxu@SPARE04>
2017-12-04 15:00:49 -0400

    not necessary to turn the timezone?

--- iBCN/Message/Entity/DateTime.cs
+++ iBCN/Message/Entity/DateTime.cs
@@ -24,7 +24,8 @@ namespace Metocean.iBCN.Message.Entity
         public override void FromBytes(byte[] entityData)
         {
             base.FromBytes(entityData);
-            Date_Time = TimeZoneInfo.ConvertTime((new System.DateTime(1980, 1, 6)).AddSeconds(BitConverter.ToUInt32(entityData.Reverse().ToArray(), 0)), TimeZoneInfo.Utc, TimeZoneInfo.Local);
+            //Date_Time = TimeZoneInfo.ConvertTime((new System.DateTime(1980, 1, 6)).AddSeconds(BitConverter.ToUInt32(entityData.Reverse().ToArray(), 0)), TimeZoneInfo.Utc, TimeZoneInfo.Local);
+            Date_Time = (new System.DateTime(1980, 1, 6)).AddSeconds(BitConverter.ToUInt32(entityData.Reverse().ToArray(), 0));
         }
     }
 }
